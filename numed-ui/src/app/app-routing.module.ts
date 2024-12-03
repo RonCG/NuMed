cd85d5678494d834228003288e96348792570ecf
Ronny Cordova <rcordova@webcreek.com>
2024-12-02 19:04:57 -0500

    added basic structure

--- numed-ui/src/app/app-routing.module.ts
+++ numed-ui/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { MedDetailComponent } from './components/med-detail/med-detail.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent},
+  { path: 'detail', component: MedDetailComponent},
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ numed-ui/src/app/components/med-detail/med-detail.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-med-detail',
+  templateUrl: './med-detail.component.html',
+  styleUrls: ['./med-detail.component.scss']
+})
+export class MedDetailComponent {
+
+}
