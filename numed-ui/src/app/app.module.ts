ccc67307fe1ab23ad957a389859502020f2aeb9d
Ronny Cordova <rcordova@webcreek.com>
2024-12-07 12:21:38 -0500

    med-detail basic structure

--- numed-ui/src/app/app.module.ts
+++ numed-ui/src/app/app.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { MedDetailComponent } from './components/med-detail/med-detail.component';
 import { AutoCompleteModule } from 'primeng/autocomplete';
-import { FormsModule } from '@angular/forms';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { PanelModule } from 'primeng/panel';
+
 
 @NgModule({
   declarations: [
@@ -20,7 +21,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     FormsModule,
     BrowserAnimationsModule,
     AppRoutingModule,
-    AutoCompleteModule
+    AutoCompleteModule,
+    PanelModule
   ],
   providers: [],
   bootstrap: [AppComponent]
